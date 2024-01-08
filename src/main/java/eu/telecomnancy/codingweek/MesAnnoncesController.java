6657ecf5339f24a14ad179c4f8821df65884c943
Helena <helena.barbillon@orange.fr>
2024-01-08 18:28:14 +0100

    affichage des annonces

--- src/main/java/eu/telecomnancy/codingweek/MesAnnoncesController.java
+++ src/main/java/eu/telecomnancy/codingweek/MesAnnoncesController.java
@@ -1,12 +1,29 @@
 package eu.telecomnancy.codingweek;
 
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class MesAnnoncesController {
     
+    @FXML
+    private VBox VBoxAnnonces;
 
     private HelloApplication app;
 
+    private ArrayList<Annonce> annonces = new ArrayList<Annonce>();
+
+    //private Annonce annonce;
+
     public MesAnnoncesController(HelloApplication app) {
         this.app = app;
+
+        //initialiser les annonces, lire dans le json
+        //exemple
+        annonces.add(new Annonce("a", "a", "a", "a", "a", "a"));
     }
 
     public void detailsAnnonce(){
@@ -15,6 +32,18 @@ public class MesAnnoncesController {
 
     public void creerAnnonce(){
         app.getSceneController().switchToCreationAnnonce();
+    }
+
+    @FXML
+    public void initialize(){
+
+        //afficher les annonces
+        for (Annonce annonce : this.annonces){
+            HBox hbox = new HBox();
+            hbox.getChildren().add(new Label(annonce.getTitre()));
+            hbox.setStyle("-fx-background-color: #eeeeee; prefHeight=\"279.0\"");
+            VBoxAnnonces.getChildren().add(hbox);
+        }
     }
 
 
