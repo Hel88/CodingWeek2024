ec7b9a9f255f34555638d1d44269ed1040df01e8
Helena <helena.barbillon@orange.fr>
2024-01-08 14:34:54 +0100

    menu bar (fxml)

--- src/main/java/eu/telecomnancy/codingweek/HelloApplication.java
+++ src/main/java/eu/telecomnancy/codingweek/HelloApplication.java
@@ -1,11 +1,11 @@
 package eu.telecomnancy.codingweek;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
