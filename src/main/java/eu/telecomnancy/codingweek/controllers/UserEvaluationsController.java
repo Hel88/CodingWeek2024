8e1712e4fa8821d611b492e0a7e5525c307fd86f
Helena <helena.barbillon@orange.fr>
2024-01-11 18:47:16 +0100

    commentaires

--- src/main/java/eu/telecomnancy/codingweek/controllers/UserEvaluationsController.java
+++ src/main/java/eu/telecomnancy/codingweek/controllers/UserEvaluationsController.java
@@ -3,6 +3,7 @@ package eu.telecomnancy.codingweek.controllers;
 import eu.telecomnancy.codingweek.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class UserEvaluationsController implements Observer{
@@ -25,14 +26,37 @@ public class UserEvaluationsController implements Observer{
 
     @Override
     public void update(String type) {
+        
         if (type.equals("evaluations")){
 
             userEvalue = app.getAnnonceAffichee().getReferent(); //valable si on est sur la page d'une annonce
-            if (type.equals("evaluations")){
-                //VBoxEvaluations.getChildren().clear();
-                username.setText(userEvalue);
-                //ajouter les evaluations à la VBox
-            }
+            //dans le cas où on est sur le profil:
+            //userEvalue = app.getMainUser().getUserName();
+
+            
+            //VBoxEvaluations.getChildren().clear();
+            username.setText(userEvalue);
+            //ajouter les evaluations à la VBox
+
+            HBox hbox = new HBox();
+            int note=4;//récupérer la note de l'utilisateur
+            Label noteCalculee = new Label(note+"");
+
+            hbox.setPrefWidth(200);
+
+            String userQuiAMisLaNote = "user1";//récupérer le nom de l'utilisateur qui a mis la note
+
+            Label userQuiAMisLaNoteLabel = new Label(userQuiAMisLaNote);
+            hbox.getChildren().add(userQuiAMisLaNoteLabel);
+
+            String commentaire = "super";
+            Label commentaireLabel = new Label(commentaire);
+            hbox.getChildren().add(commentaireLabel);
+
+            hbox.getChildren().add(noteCalculee);
+            VBoxEvaluations.getChildren().add(hbox);
+
+            
         }
         
     }
