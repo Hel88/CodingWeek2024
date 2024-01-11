1c56b838475e69160b208cda94ff70943fb89bcc
Helena <helena.barbillon@orange.fr>
2024-01-11 08:35:16 +0100

    delete classes inutiles

--- src/main/java/eu/telecomnancy/codingweek/JSONReader.java
+++
@@ -1,5 +0,0 @@
-package eu.telecomnancy.codingweek;
-
-public class JSONReader {
-    
-}
--- src/main/java/eu/telecomnancy/codingweek/utils/SupprimerAnnonce.java
+++
@@ -1,9 +0,0 @@
-package eu.telecomnancy.codingweek.utils;
-
-public class SupprimerAnnonce {
-    private Annonce annonce;
-
-    public SupprimerAnnonce(Annonce annonce){
-        this.annonce=annonce;
-    }
-}
