2de18b3a2ba7ee8abac1fcafee6dc1091ce76afa
MANGOLD Mathis <mathis.mangold@telecomnancy.net>
2024-01-12 00:25:12 +0100

    Display Report Details - Controller Done

--- src/main/java/eu/telecomnancy/codingweek/controllers/UnReportController.java
+++ src/main/java/eu/telecomnancy/codingweek/controllers/UnReportController.java
@@ -1,18 +1,58 @@
 package eu.telecomnancy.codingweek.controllers;
 
 import eu.telecomnancy.codingweek.Application;
+import eu.telecomnancy.codingweek.utils.Report;
+import eu.telecomnancy.codingweek.utils.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class UnReportController implements Observer{
 
-    private final Application app;
+    private Application app;
+    private Report report;
+
+    @FXML
+    private Label userName;
+    @FXML
+    private Label message;
+    @FXML
+    private Label date;
+    @FXML
+    private Label email;
+    @FXML
+    private Label fullName;
+
 
     public UnReportController(Application app) {
         this.app = app;
         app.addObserver(this);
     }
 
+    public void initialize() {
+        report = app.getReportAffiche();
+        if (report == null){return;}
+        userName.setText(report.getReferent());
+        message.setText(report.getMessage());
+        date.setText(report.getTime());
+
+        try {
+            User user = app.getDataUsersUtils().getUserByUserName(report.getReferent());
+            email.setText(user.getEmail());
+            fullName.setText(user.getFirstName()+" "+user.getLastName()+" ("+user.getUserName()+")");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void update(String str) {
-        // TODO Auto-generated method stub
+        if (str == "report") {
+            initialize();
+        }
     }
 }
