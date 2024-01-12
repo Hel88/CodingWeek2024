7f00b1f6649b67f27c06dc6cef02da82321ac1f5
MANGOLD Mathis <mathis.mangold@telecomnancy.net>
2024-01-12 09:01:22 +0100

    Messagerie - consulterConversation without VBox for messages

--- src/main/java/eu/telecomnancy/codingweek/controllers/ConsulterConversationController.java
+++ src/main/java/eu/telecomnancy/codingweek/controllers/ConsulterConversationController.java
@@ -1,21 +1,66 @@
 package eu.telecomnancy.codingweek.controllers;
 
 import eu.telecomnancy.codingweek.Application;
+import eu.telecomnancy.codingweek.global.Conversations;
+import eu.telecomnancy.codingweek.global.Messages;
+import eu.telecomnancy.codingweek.global.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ConsulterConversationController implements Observer {
 
     private Application app;
-    private String message;
+    private Conversations conversations;
+    private ArrayList<Messages> messages;
+
+    @FXML
+    private Label userName;
+    @FXML
+    private TextArea monMessage;
+    @FXML
+    private VBox VBoxMessages;
+
 
     public ConsulterConversationController(Application app) {
         this.app = app;
         app.addObserver(this);
     }
 
-    public void initialize() {
-//        message = app.getConversationAffichee();
-//        if (message == null){return;}
-//        //todo
+    public void initialize(){
+        conversations = app.getConversationsAffichee();
+        if (conversations == null) { return; }
+        User user = app.getMainUser();
+        if(user != null && conversations.getUser1().equals(user.getUserName())){
+            userName.setText(conversations.getUser2());
+        }
+        else if(user != null && conversations.getUser2().equals(user.getUserName())){
+            userName.setText(conversations.getUser1());
+        }
+        else if (user == null){
+            try {
+                throw new IOException("User not connected");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            try {
+                throw new IOException("User not in conversation");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            messages = app.getDataMessagesUtils().getMessagesFromConversation(String.valueOf(conversations.getId()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -23,5 +68,15 @@ public class ConsulterConversationController implements Observer {
         if(type == "conversation"){
             initialize();
         }
+    }
+
+    @FXML
+    public void send(){
+        try {
+            app.getDataMessagesUtils().addMessage(monMessage.getText(), app.getMainUser().getUserName(), String.valueOf(conversations.getId()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        app.notifyObservers("conversation");
     }
 }
