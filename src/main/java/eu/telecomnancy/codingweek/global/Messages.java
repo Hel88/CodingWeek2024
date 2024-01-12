b2769349e9c044869863e04e5617206434e8d267
MANGOLD Mathis <mathis.mangold@telecomnancy.net>
2024-01-12 01:23:37 +0100

    Messagerie - messages.json struct udpate

--- src/main/java/eu/telecomnancy/codingweek/global/Messages.java
+++ src/main/java/eu/telecomnancy/codingweek/global/Messages.java
@@ -5,13 +5,15 @@ public class Messages {
     private int id;
     private String message;
     private String expediteur;
+    private int idConversation;
 
     public Messages() {}
 
-    public Messages(int id, String message, String expediteur) {
+    public Messages(int id, String message, String expediteur, int idConversation) {
         this.id = id;
         this.message = message;
         this.expediteur = expediteur;
+        this.idConversation = idConversation;
     }
 
     public int getId() {
@@ -36,5 +38,13 @@ public class Messages {
 
     public void setExpediteur(String expediteur) {
         this.expediteur = expediteur;
+    }
+
+    public int getIdConversation() {
+        return idConversation;
+    }
+
+    public void setIdConversation(int idConversation) {
+        this.idConversation = idConversation;
     }
 }
